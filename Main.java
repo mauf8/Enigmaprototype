ef25f3c73725f8d71839721f47ebcc474288d04f
mauf8 <marc032n@stud.kea.dk>
2022-02-28 20:34:38 +0100

    test 1

---
+++ Main.java
@@ -0,0 +1,65 @@
+package demo;
+
+public class Main {
+
+
+    public void krypter() {
+        for ( char a = 'A'; a <= 'Z'; a++)
+            System.out.println(a - 'A');
+    }
+
+    public void krypter1() {
+        char a=1;
+        char b=2;
+        char c=3;
+        char d=4;
+        char e=5;
+        char f=6;
+        char g=7;
+        char h=8;
+        char i=9;
+        char j=10;
+        char k=11;
+        char l=12;
+        char m =13;
+        char n = 14;
+        char o = 15;
+        char p = 16;
+        char q = 17;
+        char r = 18;
+        char s = 19;
+        char t = 20;
+        char u = 21;
+        char v = 22;
+        char w = 23;
+        char x = 24;
+        char y = 25;
+        char z = 26;
+        char æ = 27;
+        char ø = 28;
+        char å = 29;
+
+
+    }
+
+
+    void go() {
+        krypter1();
+        krypter2();
+
+    }
+
+    public String krypter2() {
+        char [] aplhabet = "ABCDEFGHIJKLNMOPQRSTUVWXZÆØÅ".toCharArray();
+        int i = 0;
+        if (i > 29) {
+            return null;
+        }
+        return Character.toString(aplhabet[i]);
+        System.out.println(a);
+    }
+
+    public static void main(String[] args) {
+        new Main().go();
+    }
+}
